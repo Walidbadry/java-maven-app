94c357e08b39f58e3f63d07a025975ce9fbed843
walid_badry <127159054+Walidbadry@users.noreply.github.com>
2024-09-14 18:10:18 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,20 @@
 def buildJar() {
     echo "building the application..."
-    sh 'mvn package'
+    bat 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t walid123321/java_app:1.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push walid123321/java_app:1.0'
+        bat 'docker build -t walid123321/java_app:1.0 .'  // Build Docker Image
+        bat 'echo %PASS% | docker login -u %USER% --password-stdin'  // Login to Docker
+        bat 'docker push walid123321/java_app:1.0'  // Push Docker Image
     }
 } 
 
 def deployApp() {
     echo 'deploying the application...'
+    // Add Windows-specific deployment commands if needed
 } 
 
 return this
