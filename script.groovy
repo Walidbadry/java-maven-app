a17dce53dbd1e7d83ebf2d76b393b0cf096b7744
walid_badry <127159054+Walidbadry@users.noreply.github.com>
2024-09-15 17:01:11 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,16 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+// def buildJar() {
+//     echo "building the application..."
+//     sh 'mvn package'
+// } 
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t walid123321/java_app:1.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push walid123321/java_app:1.0'
-    }
-} 
+// def buildImage() {
+//     echo "building the docker image..."
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t walid123321/java_app:1.0 .'
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push walid123321/java_app:1.0'
+//     }
+// } 
 
 def deployApp() {
     echo 'deploying the application...'
