bd4c0ae28eff7cfaace3b9042fb28b7a2cbafe24
walid_badry <127159054+Walidbadry@users.noreply.github.com>
2024-09-19 22:49:41 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -16,9 +16,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t walid123321/demo-app:$IMAGE_NAME ."
+        sh "docker build -t walid123321/java-app:$IMAGE_NAME ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push walid123321/demo-app:$IMAGE_NAME"
+        sh "docker push walid123321/java-app:$IMAGE_NAME"
     }
 } 
 
