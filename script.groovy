ce31170a8bc0cf6d537d51550be81516acadf03c
walid_badry <127159054+Walidbadry@users.noreply.github.com>
2024-10-03 05:12:02 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,17 +8,17 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'docker_docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         def buildResult = bat(script: 'docker build -t walid123321/java_app:1.2 .', returnStatus: true)
         if (buildResult != 0) {
-            error "Docker build failed with status ${buildResult}"
+            error "Docker build failed with status buld${buildResult}"
         }
 
         def loginResult = bat(script: 'echo %PASS% | docker login -u %USER% --password-stdin', returnStatus: true)
         if (loginResult != 0) {
-            error "Docker login failed with status ${loginResult}"
+            error "Docker login failed with status login ${loginResult}"
         }
 
         def pushResult = bat(script: 'docker push --quiet walid123321/java_app:1.2', returnStatus: true)
         if (pushResult != 0) {
-            error "Docker push failed with status ${pushResult}"
+            error "Docker push failed with status puch ${pushResult}"
         }
     }
 }
