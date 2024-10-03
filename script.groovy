6092cd97639d48cba2560ac3d0e5b0bcc31da80a
walid_badry <127159054+Walidbadry@users.noreply.github.com>
2024-10-03 04:52:16 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,8 +4,8 @@ def buildJar() {
 }
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhup', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    echo "Building the Docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         bat """
             docker build -t walid123321/java_app:1.2 .
             echo %PASS% | docker login -u %USER% --password-stdin
